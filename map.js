42ea5d9d2d588bf1c4be7099be208a7eeee5beca
Justin Fung <justinfung.ca@gmail.com>
2025-06-04 13:34:45 -0500

    Locations not showing, trying to push

--- map.js
+++ map.js
@@ -19,7 +19,7 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
 }).addTo(map);
 
 const markerIcons = {
-  "Emergency S": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+  "Emergency Shelter": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
   "Donation Point": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
   "Community Event": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
   "Animal Shelter": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png",
@@ -56,6 +56,7 @@ function fetchResources() {
   fetch("https://opensheet.elk.sh/1saesqAewFioAJss_sjoYfVwYNuTCiWlgSoMWLiYw8WE/Sheet1")
     .then((res) => res.json())
     .then((data) => {
+       console.log("Fetched data:", data);
       const grouped = {};
 
       data.forEach((row) => {
